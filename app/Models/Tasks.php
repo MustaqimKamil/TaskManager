febe0761bd42679cd965324badb309aef7e0ab32
Amirul Mustaqim bin Kamil <amirul01mustaqim@gmail.com>
2025-05-27 00:14:35 +0800

    Create: migration and model for table 'tasks' and 'lists'

---
+++ app/Models/Tasks.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Tasks extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'is_completed',
+        'due_date',
+        'list_id'
+    ];
+
+    protected $casts = [
+        'is_completed' => 'boolean',
+        'due_date' => 'date'
+    ];
+
+    public function list(): BelongsTo
+    {
+        return $this->belongsTo(Lists::class, 'list_id');
+    }
+}
